0294d9d0a88594072baf74dbb50ff3f3cc3b2e55
sonyohann <93103346+sonyohann@users.noreply.github.com>
2021-11-11 08:32:20 +0900

    Create BOJ_14645.cpp

---
+++ Bronze 5/BOJ_14645.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	
+	int n, k, i;
+	scanf("%d %d", &n, &k);
+	
+	int a, b;
+	
+	for(i = 0; i < n; i++){
+		scanf("%d %d", &a, &b);
+	}
+	
+	printf("ºñ¿ÍÀÌ");
+	
+	return 0;
+}
