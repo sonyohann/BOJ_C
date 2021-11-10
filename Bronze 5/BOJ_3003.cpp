d5bfaac605fe527d25902676f8aa482fdbb65ab0
sonyohann <93103346+sonyohann@users.noreply.github.com>
2021-11-10 17:57:43 +0900

    Create BOJ_3003.cpp

---
+++ Bronze 5/BOJ_3003.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	int arr[6] = {1, 1, 2, 2, 2, 8};
+	int arr2[10] = {};
+	int i;
+	
+	for(i = 0; i < 6; i++){
+		scanf("%d", &arr2[i]);
+	}
+	
+	for(i = 0; i < 6; i++){
+		printf("%d ", arr[i] - arr2[i]);
+	}
+
+	return 0;
+}
