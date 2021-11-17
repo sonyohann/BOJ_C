919ce17a91176d9fdc0245b3b54c3a62fa1b7d0d
sonyohann <93103346+sonyohann@users.noreply.github.com>
2021-11-17 10:35:37 +0900

    Bronze 4

---
+++ Bronze 4/BOJ_1712.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	
+	long long a, b, c;
+	scanf("%lld %lld %lld", &a, &b, &c);
+	
+	if(b >= c){
+		printf("-1");
+	} else {
+		printf("%lld", a / (c - b) + 1);
+	}
+	
+	
+	
+	return 0;
+}
